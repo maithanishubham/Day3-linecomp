6ef6d217ca132d813ffae458895c8dc482c80eee
shubham maithani <shubhammaithani1@gmail.com>
2022-09-18 04:35:49 -0700

    Added: Using oops CompareTo and Equals to method

--- LineComparision.java
+++ LineComparision.java
@@ -8,14 +8,16 @@ public class LineComparision {
 	public static void main(String[] args) {
 
 		System.out.println("Welcome to Line Comparision");
-		LineComparision method = new LineComparision();
-		method.Length();
+		LineComparision Compare = new LineComparision();
+
+		Compare.Lengthc();
+
 	}
 
-	public void Length() {
+	public void Lengthc() {
 		Double distance_a, distance_b;
 		x1 = 1;
-		y1 = 6;
+		y1 = 10;
 		x2 = 5;
 		y2 = 6;
 		a1 = 1;
@@ -29,12 +31,11 @@ public class LineComparision {
 		distance_b = Math.sqrt(Math.pow((a2 - a1), 2) + (Math.pow((b2 - b1), 2)));
 		System.out.println("Calculated len2:" + distance_b);
 
-		if (distance_a.compareTo(distance_b) == 0) {
+		if (distance_a.equals(distance_b)) {
 			System.out.println("Line1 is  Equal to Line2");
 
 		} else if (distance_a.compareTo(distance_b) == -1) {
 			System.out.println("Line1 less than Line2");
-
 		} else if (distance_a.compareTo(distance_b) == +1) {
 			System.out.println("Line1 Greater than Line2");
 		}
